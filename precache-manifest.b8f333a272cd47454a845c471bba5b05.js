e1cea4e393fabfc51b32c314b01cd3fd850b74e0
Rehan Mohiuddin <rehan.4942@gmail.com>
2020-05-11 03:53:57 +0530

    Updates

---
+++ precache-manifest.b8f333a272cd47454a845c471bba5b05.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef3ba0980702944304ffb3ee06faab1",
+    "url": "/rivi/index.html"
+  },
+  {
+    "revision": "177c6ce9ec5e6c67e838",
+    "url": "/rivi/static/css/2.8cb4caa0.chunk.css"
+  },
+  {
+    "revision": "31be2e69f3cef4ffabbe",
+    "url": "/rivi/static/css/main.416da3b8.chunk.css"
+  },
+  {
+    "revision": "177c6ce9ec5e6c67e838",
+    "url": "/rivi/static/js/2.7832bfdc.chunk.js"
+  },
+  {
+    "revision": "328a91a19b99485dd35ad787f2912cf8",
+    "url": "/rivi/static/js/2.7832bfdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31be2e69f3cef4ffabbe",
+    "url": "/rivi/static/js/main.634962bc.chunk.js"
+  },
+  {
+    "revision": "68be8c75e3bf81553fd7c35052453a64",
+    "url": "/rivi/static/js/main.634962bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85187f22ab70c8799dfe",
+    "url": "/rivi/static/js/runtime-main.c9526ee6.js"
+  },
+  {
+    "revision": "dc0463953775a817ceb9bad631249462",
+    "url": "/rivi/static/media/Group 359.dc046395.png"
+  },
+  {
+    "revision": "42c6324f6695074cb93889e0da2221c6",
+    "url": "/rivi/static/media/Web 1280 – 1.42c6324f.png"
+  },
+  {
+    "revision": "2ae3f9ecc812ca146d952de9c8e8c0ba",
+    "url": "/rivi/static/media/denys.2ae3f9ec.jpg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/rivi/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "182796a27d309b897e77f616df128522",
+    "url": "/rivi/static/media/fabien-bazanegue.182796a2.jpg"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/rivi/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/rivi/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "c085e83e15808eff561f13f30ce937c2",
+    "url": "/rivi/static/media/landing-page.c085e83e.png"
+  },
+  {
+    "revision": "3a749d183532590fda1c14345b6ccca6",
+    "url": "/rivi/static/media/mark-finn.3a749d18.jpg"
+  },
+  {
+    "revision": "be4a1748d0f94c35e0a661164495d160",
+    "url": "/rivi/static/media/mike.be4a1748.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/rivi/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/rivi/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/rivi/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/rivi/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/rivi/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/rivi/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/rivi/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/rivi/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "2515c8e5bccbb8334071a0cce1bb2540",
+    "url": "/rivi/static/media/profile-page.2515c8e5.png"
+  },
+  {
+    "revision": "79c1998fb18c2d0078bee11f902364e3",
+    "url": "/rivi/static/media/square-purple-1.79c1998f.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/rivi/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/rivi/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/rivi/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/rivi/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/rivi/static/media/square5.ff004a0e.png"
+  }
+]);
